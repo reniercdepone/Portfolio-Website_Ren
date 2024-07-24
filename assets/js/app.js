d10339133f45f9a4dff9e41d9e3bfcf9fa4d941d
reniercdepone <33774734+reniercdepone@users.noreply.github.com>
2024-07-24 23:06:58 +0400

    Updated Links for social icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -122,7 +122,7 @@ let footer = $(`
 
     <div class="rounded-social-buttons tag">
     
-    <a class="social-button linkedin" href="" target="_blank" >
+    <a class="social-button linkedin" href="https://www.linkedin.com/in/john-renier-angelo-depone-520309139/" target="_blank" >
     <!-- svg code for linkedin icon -->
       <svg class="linkedin-icon-footer" xmlns="http://www.w3.org/2000/svg" width="35" viewBox="0 0 24 24" fill="#0e76a8"
         stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-linkedin">
@@ -132,7 +132,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button instagram" href="" target="_blank" >
+    <a class="social-button instagram" href="#" target="_blank" >
     <!-- svg code for instagram icon -->
       <svg class="instagram-icon-footer" x="0px" y="0px" viewBox="0 0 202.5 202.5"
         style="enable-background:new 0 0 202.5 202.5;">
@@ -143,7 +143,7 @@ let footer = $(`
       </svg>
     </a>
 
-    <a class="social-button github" href="#" target="_blank">
+    <a class="social-button github" href="https://github.com/reniercdepone" target="_blank">
     <!-- SVG code for Github icon -->
     <svg class="github-icon-footer" width="45px" height="45px" viewBox="0 0 300 300">
         <!-- body -->
